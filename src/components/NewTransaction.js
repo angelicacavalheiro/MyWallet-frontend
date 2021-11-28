0c97f6923206b4c5a6f4500808da12486b178d2b
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-11-28 13:31:30 -0300

    prevents letters in value input

--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -14,7 +14,7 @@ export default function NewTransaction(){
     const history = useHistory()
     let input, output = false
 
-    function requestTransaction(event) {    
+    function requestTransaction(event) {
 
         event.preventDefault();
 
@@ -26,68 +26,68 @@ export default function NewTransaction(){
         } else {
             input = false
             output = true
-        }     
+        }
 
         if(value <= 0){
             setLoading(false)
             return alert("o valor precisa ser maior que zero")
-        }  
+        }
 
         const config = {
             headers:{
                 Authorization: `Bearer ${user.token}`
             }
-        }        
+        }
 
         const body = {
-            valor: parseFloat(value).toFixed(2).replace(',', '.'), 
-            entrada: input, 
-            saida: output, 
+            valor: parseFloat(value).toFixed(2).replace(',', '.'),
+            entrada: input,
+            saida: output,
             descricao: description
         }
-    
+
         axios.post('https://mywallet-driven.herokuapp.com/movimento', body, config)
         .then(res => {
             setLoading(false)
             history.push('/transactions')
-        })  
+        })
         .catch(err => {
             setLoading(false)
-            console.log(err)        
+            console.log(err)
         })
     }
-    
+
     return(
         <Container>
         <Title> Nova
         {(transactionType == "input") ?
-                " entrada" 
+                " entrada"
                 :
-                " saída"        
-                } 
-        </Title>      
+                " saída"
+                }
+        </Title>
         <form onSubmit={requestTransaction}>
-            <input type="number" min="1" step="any" value="input" required placeholder="Valor" 
+            <input type="number" pattern='^[1-9]\d{0,2}(\.\d{3})*,\d{2}$' min="1" step="any" value="input" required placeholder="Valor"
             value={value} onChange={(e) => setValue(e.target.value)}/>
 
-            <input type="text" description="input" placeholder="Descrição" 
-            value={description} onChange={(e) => setDescription(e.target.value)}/>        
+            <input type="text" description="input" placeholder="Descrição"
+            value={description} onChange={(e) => setDescription(e.target.value)}/>
 
-            {(loading === true) ? 
+            {(loading === true) ?
             <button> <Loader type="ThreeDots" color="#FFFFFF" height={45} width={80} /> </button>
-             : 
+             :
             <button onClick={requestTransaction}> Salvar {(transactionType == "input") ? "entrada" : "saída"} </button>}
-        </form>               
+        </form>
     </Container>
     )
 }
 
 const Container = styled.div`
     display: flex;
-    flex-direction: column;	
+    flex-direction: column;
     width: 100vw;
     height: 100vh;
-    background: #8C11BE;    
+    background: #8C11BE;
 
     p {
         font-family: Raleway;
@@ -111,8 +111,8 @@ const Container = styled.div`
         font-size: 20px;
         line-height: 23px;
         color: #000000;
-        opacity: ${props => props.loading ? 0.7 : 1};    
-        background: ${props => props.loading ? "#F2F2F2" : "#FFFFFF"};  
+        opacity: ${props => props.loading ? 0.7 : 1};
+        background: ${props => props.loading ? "#F2F2F2" : "#FFFFFF"};
         pointer-events: ${props => props.loading ? "none" : "visiblePainted"};
     }
     button{
